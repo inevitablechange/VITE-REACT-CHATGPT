3ab52e8b79d62de9b0203e6769a350a04ca2cd12
inevitablechange <yongho7783@gmail.com>
2024-05-16 16:35:19 +0900

    chatlist, loading

--- src/pages/ChatList.jsx
+++ src/pages/ChatList.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import ChatlistCard from "../components/ChatListCard";
+
 const ChatList = () => {
-  return <div>ChatList</div>;
+  const [chatlist, setChatlist] = useState([]);
+
+  useEffect(() => {
+    const savedChatlist = localStorage.getItem("savedChatlist");
+
+    if (!savedChatlist) return;
+
+    setChatlist(JSON.parse(savedChatlist));
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center mt-8">
+      <ul className="mt-8 px-4">
+        {chatlist.map((v, i) => (
+          <ChatlistCard key={i} question={v.question} answer={v.answer} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ChatList;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,22 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaSpinner } from "react-icons/fa";
 import ChatlistCard from "../components/ChatListCard";
+import { stringify } from "postcss/lib/postcss";
 
 const Home = () => {
   const [content, setContent] = useState("");
   const [chatlist, setChatlist] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   const onSubmitChat = async (e) => {
     try {
       e.preventDefault();
+      setContent("");
 
       if (!content) return;
+
+      setLoading(true);
 
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
@@ -31,43 +36,66 @@ const Home = () => {
           },
         }
       );
-      const copy = [
-        ...chatlist,
-        {
-          question: content,
-          answer: response.data.choices[0].message.content,
-        },
-      ];
 
-      setChatlist(copy);
+      const newChat = {
+        question: content,
+        answer: response.data.choices[0].message.content,
+      };
+
+      //localStorage를 사용하는 것이기 때문에 let을 사용해도 된다.
+      let savedChatlist = localStorage.getItem("savedChatlist");
+
+      if (!savedChatlist) {
+        savedChatlist = [];
+      } else {
+        savedChatlist = JSON.parse(savedChatlist);
+      }
+
+      savedChatlist.push(newChat);
+
+      localStorage.setItem("savedChatlist", JSON.stringify(savedChatlist));
+
+      setChatlist([newChat, ...chatlist]);
+
+      setLoading(false);
     } catch (error) {
       console.log(error);
+
+      setLoading(false);
     }
   };
-
-  useEffect(() => {
-    console.log(chatlist);
-  }, [chatlist]);
 
   return (
     <div className="flex flex-col justify-center mt-8">
       <form className="flex justify-center" onSubmit={onSubmitChat}>
-        <input
-          className="text-2xl p-2 focus:outline-none rounded-lg border-2 border-pink-200 focus:border-pink-400"
-          type="text"
-          value={content}
-          onChange={(e) => {
-            setContent(e.target.value);
-          }}
-        />
-        <button
-          className="flex justify-center items-center ml-4 text-2xl px-4 py-[10px] rounded-full shadow-md shadow-pink-200 bg-pink-400 hover:bg-pink-500"
-          type="submit"
-        >
-          <FaSearch />
-          검색
-        </button>
+        {isLoading ? (
+          <FaSpinner className="animate-spin-slow" />
+        ) : (
+          <>
+            <input
+              className="text-2xl p-2 focus:outline-none rounded-lg border-2 border-pink-200 focus:border-pink-400"
+              type="text"
+              value={content}
+              // disabled={isLoading} isloading으로 로딩 중에는 검색 막는 방법
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+            />
+            {isLoading ? (
+              <FaSpinner />
+            ) : (
+              <button
+                className="flex justify-center items-center ml-4 text-2xl px-4 py-[10px] rounded-full shadow-md shadow-pink-200 bg-pink-400 hover:bg-pink-500"
+                type="submit"
+              >
+                <FaSearch />
+                검색
+              </button>
+            )}
+          </>
+        )}
       </form>
+
       <ul className="mt-8 px-4">
         {chatlist.map((v, i) => (
           <ChatlistCard key={i} question={v.question} answer={v.answer} />
