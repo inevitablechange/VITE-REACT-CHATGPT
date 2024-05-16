a6b10dc2b941611e2abae8c1464b977dca33c773
inevitablechange <yongho7783@gmail.com>
2024-05-16 13:45:29 +0900

    chatlist

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,18 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
+import ChatlistCard from "../components/ChatListCard";
 
 const Home = () => {
   const [content, setContent] = useState("");
+  const [chatlist, setChatlist] = useState([]);
 
   const onSubmitChat = async (e) => {
     try {
       e.preventDefault();
+
+      if (!content) return;
+
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
         {
@@ -15,7 +20,7 @@ const Home = () => {
           messages: [
             {
               role: "user",
-              content: "오늘 점심 추천해줘",
+              content,
             },
           ],
         },
@@ -26,15 +31,27 @@ const Home = () => {
           },
         }
       );
-      console.log(response);
+      const copy = [
+        ...chatlist,
+        {
+          question: content,
+          answer: response.data.choices[0].message.content,
+        },
+      ];
+
+      setChatlist(copy);
     } catch (error) {
       console.log(error);
     }
   };
 
+  useEffect(() => {
+    console.log(chatlist);
+  }, [chatlist]);
+
   return (
-    <div className="flex justify-center mt-8">
-      <form className="flex" onSubmit={onSubmitChat}>
+    <div className="flex flex-col justify-center mt-8">
+      <form className="flex justify-center" onSubmit={onSubmitChat}>
         <input
           className="text-2xl p-2 focus:outline-none rounded-lg border-2 border-pink-200 focus:border-pink-400"
           type="text"
@@ -51,6 +68,11 @@ const Home = () => {
           검색
         </button>
       </form>
+      <ul className="mt-8 px-4">
+        {chatlist.map((v, i) => (
+          <ChatlistCard key={i} question={v.question} answer={v.answer} />
+        ))}
+      </ul>
     </div>
   );
 };
